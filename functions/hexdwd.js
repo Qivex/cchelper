c27a696f252ba0e39709002db17338e8ffd76654
Qivex <qivex@web.de>
2021-03-14 19:20:49 +0100

    Wrapped everything in try-block

--- functions/hexdwd.js
+++ functions/hexdwd.js
@@ -1,20 +1,22 @@
 const https = require("https")
 
 exports.handler = async (event, context) => {
-	const url = event.body
-	
-	const promise = new Promise(function(resolve, reject) {
-		https.get(url, response => {
-			if (response.statusCode === 200) {
-				let data = ""
-				response.setEncoding("hex")
-				response.on("data", chunk => { data += chunk })
-				response.on("end", () => { resolve({statusCode: 200, body: data}) })
-			} else {
-				resolve({statusCode: 404})
-			}
-		}).on('error', err => { reject() })
-	})
-	
-	return promise
+	try {
+		const url = new URL(event.body)
+		const promise = new Promise(function(resolve, reject) {
+			https.get(url, response => {
+				if (response.statusCode === 200) {
+					let data = ""
+					response.setEncoding("hex")
+					response.on("data", chunk => { data += chunk })
+					response.on("end", () => { resolve({statusCode: 200, body: data}) })
+				} else {
+					resolve({statusCode: 404})
+				}
+			})
+		})
+		return promise
+	} catch (e) {
+		return {statusCode: 404}
+	}
 }
